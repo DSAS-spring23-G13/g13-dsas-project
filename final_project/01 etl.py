9fa2bfd8ad1e694f5c018b452a2d8519541e7798
thapasya-m <thapasya-m>
2023-04-15 18:21:36 +0000

    historic bike trip load

--- final_project/01 etl.py
+++ final_project/01 etl.py
@@ -22,7 +22,7 @@ print(start_date,end_date,hours_to_forecast, promote_model)
 
 # MAGIC %md
 # MAGIC ## load historic data
-# MAGIC BIKE_TRIP_DATA_PATH
+# MAGIC 
 # MAGIC NYC_WEATHER_FILE_PATH
 
 # COMMAND ----------
@@ -52,10 +52,72 @@ display(weather_df)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## load historic data
+# MAGIC 
+# MAGIC BIKE_TRIP_DATA_PATH
+
+# COMMAND ----------
+
+import os
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, TimestampType
+
+# Get a list of all CSV files in the directory
+csv_files = [os.path.join(BIKE_TRIP_DATA_PATH, f.name) for f in dbutils.fs.ls(BIKE_TRIP_DATA_PATH) if f.name.endswith('.csv')]
+
+# Define the schema for the DataFrame
+schema = StructType([
+    StructField("ride_id", StringType(), True),
+    StructField("rideable_type", StringType(), True),
+    StructField("start_station_name", StringType(), True),
+    StructField("start_station_id", StringType(), True),
+    StructField("end_station_name", StringType(), True),
+    StructField("end_station_id", StringType(), True),
+    StructField("member_casual", IntegerType(), True),
+    StructField("started_at", TimestampType(), True),
+    StructField("ended_at", TimestampType(), True),
+    StructField("end_lng", DoubleType(), True),
+    StructField("end_lat", DoubleType(), True),
+    StructField("start_lng", DoubleType(), True),
+    StructField("start_lat", DoubleType(), True),
+])
+
+# [ride_id: string, rideable_type: string, started_at: timestamp, ended_at: timestamp, start_station_name: string, start_station_id: string, end_station_name: string, end_station_id: string, start_lat: double, start_lng: double, end_lat: double, end_lng: double, ]
+
+
+# Create an empty DataFrame
+df = spark.createDataFrame([], schema)
+
+# Loop through the CSV files and append them to the DataFrame
+for file in csv_files:
+    temp_df = spark.read.format('csv').option("inferSchema","True").option('header', 'true').load(file)
+#     temp_df = temp_df.withColumn('source_file', input_file_name())
+    df = df.unionByName(temp_df)
+
+# COMMAND ----------
+
+# Write the DataFrame to a Delta table
+# Write the processed data to a delta table
+output_path = GROUP_DATA_PATH + "historic_bike_trips"
+
+if not os.path.isdir(output_path):
+    dbutils.fs.mkdirs(output_path)
+
+df.write.format("delta").mode("overwrite").save(output_path)
+
+df.write.format("delta").mode("overwrite").saveAsTable("historic_bike_trips")
+
+# COMMAND ----------
+
+# bdf=spark.read.format('csv').option("header","True").option("inferSchema","True").load(BIKE_TRIP_DATA_PATH+'202111_citibike_tripdata.csv')
+display(df)
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC use g13_db;
 # MAGIC 
-# MAGIC select top from historic_weather_info;
+# MAGIC select * from historic_bike_trips limit 10;
 
 # COMMAND ----------
 
@@ -64,7 +126,7 @@ display(weather_df)
 
 # COMMAND ----------
 
-display(dbutils.fs.ls('dbfs:/FileStore/tables/'))
+display(dbutils.fs.ls('dbfs:/FileStore/tables/G13/historic_weather'))
 # display(dbutils.fs.rm('dbfs:/FileStore/tables/G13/historic_weather', recurse = True))
 
 # COMMAND ----------
