00c8c5acf3ef96616298448dec34c3442734512a
arunaggiri.pandian@gmail.com <arunaggiri.pandian@gmail.com>
2023-04-27 23:13:59 +0000

    Just few changes

--- final_project/02 eda.py
+++ final_project/02 eda.py
@@ -346,42 +346,27 @@ print("YOUR CODE HERE...")
 
 # COMMAND ----------
 
-# NYC_WEATHER_FILE_PATH
-# BIKE_TRIP_DATA_PATH
-# BRONZE_STATION_INFO_PATH
-# BRONZE_STATION_STATUS_PATH
-# BRONZE_NYC_WEATHER_PATH
 
-old_weather_df = spark.read.format("csv").option("header", "true").load(NYC_WEATHER_FILE_PATH)
-display(old_weather_df)
 
 # COMMAND ----------
 
-old_weather_df.printSchema()
+
 
 # COMMAND ----------
 
-old_bike_info_fs = [file.path for file in dbutils.fs.ls(BIKE_TRIP_DATA_PATH) if file.name.endswith("_citibike_tripdata.csv")]
 
-old_bike_info_fs
 
 # COMMAND ----------
 
-# lets start with last years trend
-oldb_df = spark.read.format("csv").option("header", "true").load('dbfs:/FileStore/tables/raw/bike_trips/202301_citibike_tripdata.csv')
-display(oldb_df)
+
 
 # COMMAND ----------
 
-# unique rideable
-unique_values = [row.rideable_type for row in oldb_df.select("rideable_type").distinct().collect()]
-# unique_values # ['docked_bike', 'classic_bike', 'electric_bike']
 
-# how about some commonly start start_station_name
 
 # COMMAND ----------
 
-dbutils.fs.ls(BIKE_TRIP_DATA_PATH)
+
 
 # COMMAND ----------
 
